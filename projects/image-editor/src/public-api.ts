fe150794cc21fe36dcd946cc83b59db8c2399464
renan-mf-ribeiro <reribeirosantos@hotmail.com>
2023-08-20 23:24:16 -0300

    CREATE Package Image Editor

---
+++ projects/image-editor/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of image-editor
+ */
+
+export * from './lib/components/image-editor.component';
+export * from './lib/components/input-image/input-image.component';
+export * from './lib/image-editor.module';
